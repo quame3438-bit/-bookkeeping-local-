dadacfd4ed795a0a511029ee7d0067671ff5f610
quame3438-bit <quame3438@gmail.com>
2026-08-15 13:28:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const C='multi-book-v4';
-const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', () => {
+  self.clients.claim();
+});
